544da354f05acb95ac8182f6be618c257a3b8f6b
JoeDKoal94 <51380575+JoeDKoal94@users.noreply.github.com>
2021-01-12 10:31:53 -0600

    Update

--- Hotel-UserInyerFace/src/app/app.module.ts
+++ Hotel-UserInyerFace/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { NavbarComponent } from './component/navbar/navbar.component';
 import { LandingPageComponent } from './component/landing-page/landing-page.component';
 import { BookingPageComponent } from './component/booking-page/booking-page.component';
 import { CheckoutPageComponent } from './component/checkout-page/checkout-page.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { CheckoutPageComponent } from './component/checkout-page/checkout-page.c
     CheckoutPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
