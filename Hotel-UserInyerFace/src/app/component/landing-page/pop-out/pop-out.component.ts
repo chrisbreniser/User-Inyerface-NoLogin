26495abc76120da0b25156ba60295605eaec96ea
JoeDKoal94 <51380575+JoeDKoal94@users.noreply.github.com>
2021-01-19 16:45:41 -0600

    Updated landing

---
+++ Hotel-UserInyerFace/src/app/component/landing-page/pop-out/pop-out.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pop-out',
+  templateUrl: './pop-out.component.html',
+  styleUrls: ['./pop-out.component.css']
+})
+export class PopOutComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
